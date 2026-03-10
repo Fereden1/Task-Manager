da8f97a90539ed7a574bb687d43b60201a32f788
Fereden <saiffarit11@gmail.com>
2026-03-11 00:00:16 +0300

    1 homework

--- 2 sm/roles.sql
+++ 2 sm/roles.sql
@@ -1,33 +1,16 @@
-grant connect on database taskdb to admin;
-grant all privileges on schema task_manager to admin;
-grant all privileges on all tables in schema task_manager to admin;
-grant all privileges on all sequences in schema task_manager to admin;
+GRANT CONNECT ON DATABASE taskdb TO admin;
+GRANT ALL PRIVILEGES ON SCHEMA task_manager TO admin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA task_manager TO admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA task_manager TO admin;
 
-create role app login password 'app123';
-grant connect on database taskdb to app;
-grant usage on schema task_manager to app;
-grant select, insert, update, delete on all tables in schema task_manager to app;
-grant usage, select on all sequences in schema task_manager to app;
+CREATE ROLE app LOGIN PASSWORD 'app123';
+GRANT CONNECT ON DATABASE taskdb TO app;
+GRANT USAGE ON SCHEMA task_manager TO app;
+GRANT SELECT ON ALL TABLES IN SCHEMA task_manager TO app;
+GRANT INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA task_manager TO app;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA task_manager TO app;
 
-create role readonly login password 'readonly123';
-grant connect on database taskdb to readonly;
-grant usage on schema task_manager to readonly;
-grant select on all tables in schema task_manager to readonly;
-
-alter default privileges in schema task_manager
-grant all privileges on tables to admin;
-
-alter default privileges in schema task_manager
-grant all privileges on sequences to admin;
-
-alter default privileges in schema task_manager
-grant select, insert, update, delete on tables to app;
-
-alter default privileges in schema task_manager
-grant usage, select on sequences to app;
-
-alter default privileges in schema task_manager
-grant select on tables to readonly;
-
-grant app to admin;
-grant readonly to admin;
+CREATE ROLE readonly LOGIN PASSWORD 'readonly123';
+GRANT CONNECT ON DATABASE taskdb TO readonly;
+GRANT USAGE ON SCHEMA task_manager TO readonly;
+GRANT SELECT ON ALL TABLES IN SCHEMA task_manager TO readonly;
